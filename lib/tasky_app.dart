6a2e27901fe21a2af092b0a7d6c3a1b20bb5467c
Mohamed Elsayed <elmoosayed@gmail.com>
2025-09-01 19:30:50 +0300

    - run app

---
+++ lib/tasky_app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TaskyApp extends StatelessWidget {
+  const TaskyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
