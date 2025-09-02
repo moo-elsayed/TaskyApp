1da6b6c19f26031dcf9cd99fe41229010190a7e3
Mohamed Elsayed <elmoosayed@gmail.com>
2025-09-02 14:54:25 +0300

    - change app name

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:tasky_app/core/routing/app_router.dart';
 import 'package:tasky_app/tasky_app.dart';
 
 void main() {
-  runApp(TaskyApp());
+  runApp(TaskyApp(appRouter: AppRouter()));
 }
